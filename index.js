4b8079c86dab5256dbb213397e86198e471cf6d1
eluser <studenteliot1999@gmail.com>
2022-09-22 15:05:09 +0300

    Add players functionality

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+let choices = ['rock', 'paper', 'scissors'];
 function getComputerChoice(){
-    let guesses = ['rock', 'paper', 'scissors'];
-    let guess = Math.floor(Math.random(guesses.length))
+    let choice = Math.floor(Math.random(choices.length))
     let rand = Math.floor(Math.random()*3)
-    let randomNum = guesses[rand]
-    console.log(randomNum);
+    let randomNum = choices[rand]
+    console.log("Computer choice is: ", randomNum);
 }
+getComputerChoice()
+function playerChoice(){
+    let userChoice = prompt("Rock - Paper  or Scissors ?");
+
+    if(userChoice === choices[0]){
+        console.log("User choice is: ", choices[0]);
+    }else if(userChoice === choices[1]){
+        console.log("User choice is: ", choices[1]);
+    }else if(userChoice === choices[2]){
+        console.log("User choice is: ", choices[2]);
+    }else{
+        console.log('Not a choice...');
+    }
+}
+playerChoice()
